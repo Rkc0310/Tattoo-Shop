72b45e92d1b65e0de1cbcb8f4ab6aa4832d5be5d
Roshan Chnadravanshi <rkkumar966930@gmail.com>
2026-03-07 17:02:02 +0530

    Updated UI, fixed scroll issue and menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from "react-router-dom"
+import { useEffect } from 'react';
 
 const Header = () => {
       const [open, setOpen] =useState(false)
+
+    const location = useLocation()
+
+useEffect(() => {
+  setOpen(false)
+}, [location])
+
     return (
         <div>
             <header className="sticky top-0 z-50 bg-stone-950/95 backdrop-blur-sm border-b border-amber-600/20">
